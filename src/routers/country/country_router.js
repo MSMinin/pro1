514f26603b10a28e18ffc3339a39a45292b9e0cd
yujin <hina0510@naver.com>
2023-08-17 10:34:28 +0900

    header수정2

--- src/routers/country/country_router.js
+++ src/routers/country/country_router.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
 const pCtrl = require("../../cotroller/project_ctrl");
 
-router.get("/k", pCtrl);
-router.get("/k", pCtrl);
-router.get("/k", pCtrl);
+//router.get("/k", pCtrl);
+////router.get("/k", pCtrl);
+//router.get("/k", pCtrl);
 router.get("/tokyo", pCtrl.views.tokyo);
 router.get("/osaka", pCtrl.views.osaka);
 router.get("/sapporo", pCtrl.views.sapporo);
-router.get("/ch", pCtrl);
-router.get("/ch", pCtrl);
-router.get("/fr", pCtrl);
-router.get("/en", pCtrl);
-router.get("/us", pCtrl);
+//router.get("/ch", pCtrl);
+//router.get("/ch", pCtrl);
+//router.get("/fr", pCtrl);
+//router.get("/en", pCtrl);
+//router.get("/us", pCtrl);
 
 module.exports = router
